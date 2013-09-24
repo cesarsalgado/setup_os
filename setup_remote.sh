c4e5465b5fd4324129c76ee33b70ba4b0b9eac3d
Cesar Salgado <cesarsvs@gmail.com>
2013-09-23 21:56:39 -0300

    Added line to install R.

--- setup_remote.sh
+++ setup_remote.sh
@@ -9,7 +9,24 @@ fail () {
 sudo apt-get install git
 sudo apt-get install vim
 sudo apt-get install meld
+sudo apt-get install r-base r-base-dev
 
+# commands to install node.js
+sudo apt-get update
+# Install a special package
+sudo apt-get install -y python-software-properties python g++ make
+# Add a new repository for apt-get to search
+sudo add-apt-repository ppa:chris-lea/node.js
+# Update apt-get’s knowledge of which packages are where
+sudo apt-get update
+# Now install nodejs and npm
+sudo apt-get install -y nodejs
+
+# install ungit. Graphical interface to git.
+sudo npm install -g ungit
+
+
+# part below copy setup the dotfiles.
 cd $HOME
 
 # git pull and install dotfiles as well
